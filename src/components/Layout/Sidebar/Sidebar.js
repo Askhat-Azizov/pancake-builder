c47630bbd677c9a354c765e5ce9946d016c134f5
Your Name <you@example.com>
2020-04-12 20:29:57 +0600

    Add logo

--- src/components/Layout/Sidebar/Sidebar.js
+++ src/components/Layout/Sidebar/Sidebar.js
@@ -1,8 +1,10 @@
 import React from "react";
 import classes from "./Sidebar.module.css";
+import logo from "../../../assets/logo.svg";
 
 export default () => (
   <div className={classes.Sidebar}>
+    <img srs={logo} />
     <span>Pancake Builder</span>
   </div>
 );
