346c2669e2d07bc8f05d6f91469d8e274af19d6e
Your Name <you@example.com>
2020-04-13 11:23:47 +0600

    Added font-family ang logo

--- src/components/Layout/Sidebar/Sidebar.js
+++ src/components/Layout/Sidebar/Sidebar.js
@@ -1,8 +1,10 @@
 import React from "react";
+import logo from "../../../assets/logo.svg";
 import classes from "./Sidebar.module.css";
 
 export default () => (
   <div className={classes.Sidebar}>
+    <img src={logo} />
     <span>Pancake Builder</span>
   </div>
 );
