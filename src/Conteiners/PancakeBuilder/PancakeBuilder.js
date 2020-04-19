89b124fb60ea42bfb8e99ff68941f4830ddec4a0
Your Name <you@example.com>
2020-04-19 21:35:18 +0600

    Added price for OrderSummary

--- src/Conteiners/PancakeBuilder/PancakeBuilder.js
+++ src/Conteiners/PancakeBuilder/PancakeBuilder.js
@@ -79,6 +79,7 @@ export default function () {
           fruits={fruits}
           cancelOrder={cancelOrder}
           finishOrder={finishOrder}
+          price={price}
         />
       </Modal>
     </div>
--- src/components/PancakeBuilder/OrderSummary/OrderSumarry.js
+++ src/components/PancakeBuilder/OrderSummary/OrderSumarry.js
@@ -3,13 +3,13 @@ import classes from "./OrderSummary.module.css";
 import Button from "../../UI/Button/Button";
 
 const LABELS = {
-    banana:"Banana",
-    strawberry:"Strawberry",
-    kiwi:"Kiwi",
-    chocolate:"Chocolate"
+  banana: "Banana",
+  strawberry: "Strawberry",
+  kiwi: "Kiwi",
+  chocolate: "Chocolate",
 };
 
-export default function ({ fruits, cancelOrder, finishOrder }) {
+export default function ({ fruits, cancelOrder, finishOrder, price }) {
   const fruitsOutput = Object.keys(fruits)
     .filter((fruit) => fruits[fruit] > 0)
     .map((fruit) => (
@@ -19,12 +19,17 @@ export default function ({ fruits, cancelOrder, finishOrder }) {
     ));
   return (
     <div className={classes.orderSummary}>
-      <h2>to eat</h2>
+      <h2>To eat</h2>
       <p>and you will eat it ???</p>
       <ul>{fruitsOutput}</ul>
+      <p>Nu ty i zhadnyy zhadnyy: {price}som</p>
       <p>Would you like to checkout?</p>
-      <Button green click={finishOrder}>Checkout</Button>
-      <Button red click={cancelOrder}>Cancel</Button>
+      <Button green click={finishOrder}>
+        Checkout
+      </Button>
+      <Button red click={cancelOrder}>
+        Cancel
+      </Button>
     </div>
   );
 }
