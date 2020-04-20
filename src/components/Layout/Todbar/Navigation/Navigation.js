f42dfd1452b3c8dc30278f3057626a8546dc58e3
Your Name <you@example.com>
2020-04-20 10:17:36 +0600

    Added links for Navigation

--- src/components/Layout/Todbar/Navigation/Navigation.js
+++ src/components/Layout/Todbar/Navigation/Navigation.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import classes from './Navigation.module.css';
 
-export default ({ children }) => (
-<li>{children}</li>
+export default ({ children, url }) => (
+    <li>
+     <a href="url" >{children}</a>
+    </li>
 );
--- src/components/Layout/Todbar/Toolbar.js
+++ src/components/Layout/Todbar/Toolbar.js
@@ -3,12 +3,12 @@ import classes from "./Toolbar.module.css";
 import Sidebar from "../Sidebar/Sidebar";
 import Navigation from "./Navigation/Navigation";
 
-export default () => (
+export default ({ url }) => (
   <div className={classes.Toolbar}>
     <Sidebar />
     <ul>
-      <Navigation>Pancake-builder</Navigation>
-      <Navigation>Checkout</Navigation>
+      <Navigation url="/">Pancake-builder</Navigation>
+      <Navigation url="/checkout">Checkout</Navigation>
     </ul>
   </div>
 );
