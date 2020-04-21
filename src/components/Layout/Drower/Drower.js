cdec389829e279310cda6415f7f196990f4baff5
Your Name <you@example.com>
2020-04-21 22:51:36 +0600

    Add Logo and Navigation to Drower component

--- src/components/Layout/Drower/Drower.js
+++ src/components/Layout/Drower/Drower.js
@@ -1,8 +1,11 @@
 import React from "react";
 import classes from "./Drower.module.css";
+import Logo from "../../UI/Logo/Logo";
+import Navigation from "../Todbar/Navigation/Navigation"
 
 export default () => (
   <div className={classes.Drower}>
-    Drower
+    <Logo />
+    <Navigation/>
   </div>
 );
