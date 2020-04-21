e49fea0fcfe000cd477715072768f22abeaf2084
Your Name <you@example.com>
2020-04-21 22:15:03 +0600

    Add Drower components and styles

---
+++ src/components/Layout/Drower/Drower.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./Drower.module.css";
+
+export default () => (
+  <div className={classes.Drower}>
+    Drower
+  </div>
+);
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Toolbar from './Todbar/Toolbar';
 import classes from "./Layout.module.css";
+import Drower from "../Layout/Drower/Drower";
 
 export default function({ children }) {
     return (
        <div className={classes.Layout}>
            <Toolbar/>
+           <Drower/>
            {children}
+           
        </div>
     );
 }
