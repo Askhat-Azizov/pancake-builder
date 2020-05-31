b6ee33d6f1c004fb5384378a0a1bf033d4569564
Your Name <you@example.com>
2020-06-01 04:32:32 +0600

    Restructure Containers into components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Layout from "./Conteiners/Layout/Layout";
 import PancakeBuilder from "./Conteiners/PancakeBuilder/PancakeBuilder";
-import Checkout from "./Conteiners/Checkout/Checkout";
-import Orders from  "./Conteiners/Orders/Orders";
+import Checkout from "./components/Checkout/Checkout";
+import Orders from  "./components/Orders/Orders";
 import "./App.css";
 
 export default () => {
