76757543203a6c6e42dfb03237003814113e3733
Your Name <you@example.com>
2020-06-05 23:06:25 +0600

    Add Firebase URL

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -13,6 +13,7 @@ export const fail = (dispatch, errors) => dispatch({
   type: AUTH_FAIL, errors
 });
 
-export const auth = (dispatch, email, password) => axios.post()
+export const auth = (dispatch, email, password) => axios
+  .post("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCrBVuS3c00Psb7p7nBDahUoC0g1zqV43A", { email, password })
   .then(({ data }) => success(dispatch, data))
   .catch(error => fail(dispatch, error)); 
