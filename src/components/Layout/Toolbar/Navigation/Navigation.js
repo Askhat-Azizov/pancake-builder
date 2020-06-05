22bf435a954bb5ec313b76d5a51020220e249eb2
Your Name <you@example.com>
2020-06-06 00:03:50 +0600

    Add auth logic to Navigation

--- src/components/Layout/Toolbar/Navigation/Navigation.js
+++ src/components/Layout/Toolbar/Navigation/Navigation.js
@@ -1,11 +1,17 @@
 import React from "react";
 import NavigationItem from "./NavigationItem/NavigationItem";
 import classes from "./Navigation.module.css";
+import { useSelector } from "react-redux";
 
-export default () => (
-  <ul className={classes.Navigation}>
-    <NavigationItem url="/builder">Pancake Builder</NavigationItem>
-    <NavigationItem url="/orders">Orders</NavigationItem>
-    <NavigationItem url="/auth">Authenticate</NavigationItem>
-  </ul>
-);
+export default () => {
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
+
+  return (
+    <ul className={classes.Navigation}>
+      <NavigationItem url="/builder">Sushi Builder</NavigationItem>
+      {isAuthenticated ? <NavigationItem url="/orders">Orders</NavigationItem> : null}
+      {!isAuthenticated ? <NavigationItem url="/auth">Authenticate</NavigationItem> : null}
+      {isAuthenticated ? <NavigationItem url="/logout">Logout</NavigationItem> : null}
+    </ul>
+  );
+}
