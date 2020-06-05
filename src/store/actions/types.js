e5a5d8b3161c27f682bb3198845767a8b1a8a9c6
Your Name <you@example.com>
2020-06-05 22:21:29 +0600

    Add auth redux elements

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -3,3 +3,7 @@ export const REMOVE_FRUIT = "REMOVE_FRUIT";
 export const SET_FRUITS = "SET_FRUITS";
 
 export const SET_ORDERS = "SET_ORDERS";
+
+export const AUTH_START = "AUTH_START";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_FAIL = "AUTH_FAIL"; 
