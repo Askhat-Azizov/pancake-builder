61a54598b399995190db70d73a44287b26ebddd0
Your Name <you@example.com>
2020-06-05 22:02:41 +0600

    Fix ingredient sorting

--- src/store/reducers/builder.js
+++ src/store/reducers/builder.js
@@ -6,41 +6,28 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
+  const newState = { ...state };
+
   switch (action.type) {
     case types.ADD_FRUIT:
-      return {
-        ...state,
-        fruits: {
-          ...state.fruits,
-          [action.fruit]: { 
-          ...state.fruits[action.fruit],
-          quantity: state.fruits[action.fruit].quantity + 1,
-        },
-        },
-        price: state.price + state.fruits[action.fruit].price,
-      };
+      newState.fruits[action.fruit].quantity++;
+      newState.price = state.price + state.fruits[action.fruit].price;
+
+      return newState;
 
     case types.REMOVE_FRUIT:
-      return {
-        ...state,
-        fruits: {
-          ...state.fruits,
-          [action.fruit]: { 
-            ...state.fruits[action.fruit],
-            quantity: state.fruits[action.fruit].quantity - 1,
-          },
-        },
-        price: state.price -state.fruits[action.fruit].price,
-      };
+      newState.fruits[action.fruit].quantity--;
+      newState.price = state.price - state.fruits[action.fruit].price;
+
+      return newState;
 
       case types.SET_FRUITS:
-      return {
-        ...state,
-        fruits: action.fruits,
-        price: initialState.price
-      };
+        newState.fruits = action.fruits;
+        newState.price = initialState.price;
+  
+        return newState;
 
     default:
-      return state;
+      return newState;;
   }
 };
