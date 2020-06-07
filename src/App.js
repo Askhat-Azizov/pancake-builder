e4d09919ebadd30106d66bba93005937fe2b3f9c
Your Name <you@example.com>
2020-06-07 18:45:52 +0600

    Add token expiration to localStorage

--- src/App.js
+++ src/App.js
@@ -12,8 +12,10 @@ import { useDispatch } from "react-redux";
 
 export default () => {
   const dispatch = useDispatch();
-  useEffect(() => restore(dispatch));
-  
+  useEffect(() => {
+    restore(dispatch);
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Layout>
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -17,6 +17,19 @@ export const logout = (dispatch) => dispatch({
   type: AUTH_LOGOUT
 });
 
+export const restore = (dispatch) => {
+  const idToken = localStorage.getItem("idToken");
+  const localId = localStorage.getItem("localId");
+
+  if (idToken && localId) {
+    const expirationDate = new Date(localStorage.getItem("expirationDate"));
+    if (expirationDate > new Date()) {
+      success(dispatch, { idToken, localId });
+      timeout(dispatch, (expirationDate.getTime() - new Date().getTime()) / 1000);
+    }
+  }
+}
+
 export const timeout = (dispatch, seconds) => setTimeout(() => logout(dispatch), seconds * 1000);
 
 const key = "AIzaSyCrBVuS3c00Psb7p7nBDahUoC0g1zqV43A";
@@ -25,6 +38,8 @@ const signUpUrl = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key
 export const auth = (dispatch, method, email, password) => axios
   .post(method === "signin" ? signInUrl : signUpUrl, { email, password, returnSecureToken: true })
   .then(({ data }) => {
+    const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000);
+    localStorage.setItem("expirationDate", expirationDate);
     localStorage.setItem("idToken", data.idToken);
     localStorage.setItem("localId", data.localId);
 
@@ -32,16 +47,4 @@ export const auth = (dispatch, method, email, password) => axios
     success(dispatch, data);
     timeout(dispatch, +data.expiresIn);
   })
-  .catch(error => fail(dispatch, error));
-
-export const restore = (dispatch) => {
-  const idToken = localStorage.getItem("idToken");
-  const localId = localStorage.getItem("localId");
-
-  if (idToken && localId) {
-    success(dispatch, { idToken, localId })
-  }
-  else {
-    logout(dispatch);
-  }
-} 
+  .catch(error => fail(dispatch, error)); 
